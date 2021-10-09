72c847006594e6b561e8e5deeb8a543f800ed0c1
gitshreevatsa <vastas2002@gmail.com>
2021-10-09 16:51:52 +0530

    Resolved overlap error for post

--- student_assignment.py
+++ student_assignment.py
@@ -1,7 +1,5 @@
-
 from flask import Flask, request 
 import os , json
-from copy import deepcopy
 app = Flask(__name__)
 
 @app.route('/studentsData', methods = ['GET', 'POST', 'PUT', 'DELETE'])
@@ -15,53 +13,30 @@ def get_method():
         return f
 
 
-#@app.route('/studentsData', methods = ['GET', 'POST', 'PUT', 'DELETE'])
+#@app.route('/studentsData', methods = ['GET', 'POST', 'PUT', 'DELETE']) add new thing to whole file
     if(request.method == 'POST'):
         data = request.get_json()
+        print(list(data.keys()))
         path = os.path.join(os.getcwd(),'student.json')
         f = open(path, 'r')
         f = f.read()
         f = json.loads(f)
         a = f['students']
-        print(type(data))
-        print(type(a))
-        """
-        def dict_of_dicts_merge(a, data):
-            z = {}
-            overlapping_keys = a.items() & data.items()
-            if(a.items() == data.items()):
-                return "Error"
-            else:
-                for key in overlapping_keys:
-                    z[key] = dict_of_dicts_merge(a[key], data[key])
-                for key in a.keys() - overlapping_keys:
-                    z[key] = deepcopy(a[key])
-                for key in data.keys() - overlapping_keys:
-                    z[key] = deepcopy(data[key])
-                    print(z)
-                return z
-        lam =  dict_of_dicts_merge(a, data)
-        """
-        lam = f | data
-        new_path = os.path.join(path)
-        with open(new_path, 'w') as file:  
-            document = json.dump(lam, file)
-        return "Updated Successfully"
+        a_list = list(a)
+        print(data['id'])
+        overlap = data['id']
+        if(overlap in a_list):
+            return "Error"
+        else:
+            a[data['id']] = {"name" : data['name']}
+            new_path = os.path.join(path)
+            with open(new_path, 'w') as file:  
+                document = json.dump(f, file)
+            return "Updated Successfully"
         
-    
-    
-        
-
-
-    
-        
-            
-            
-            
-            
-
-        
-    
+ 
 
 if __name__ == '__main__':
     app.run(debug= True)
+
+
